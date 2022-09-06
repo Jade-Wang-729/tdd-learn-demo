d1acce99aaadbcabfa3d95b359ac48e708ca108d
Jade <yujue_official@163.com>
2022-09-06 09:39:54 +0800

    db: add init tables for flyway

---
+++ tdd-card-user/src/main/resources/db/migration/V202208291758__create_table_verifications.sql
@@ -0,0 +1,4 @@
+create table if not exists verifications(
+    id varchar(19) primary key,
+    verifications int default 0
+);
---
+++ tdd-card-user/src/main/resources/db/migration/V202208301358__create_table_card_user.sql
@@ -0,0 +1,5 @@
+create table if not exists cardUser(
+    id varchar(19) primary key,
+    name varchar(64) not null,
+    phone varchar(64) not null
+    );
