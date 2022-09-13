ee6b6959a8611de67205521c4ae2997a5fd5f3fb
Jade <yujue_official@163.com>
2022-09-13 17:58:09 +0800

    db: add init tables for flyway

--- tdd-card-user/src/test/java/tdd/learn/card/user/PostgreSQLTestImages.java
+++ tdd-card-user/src/test/java/tdd/learn/card/user/PostgreSQLTestImages.java
@@ -3,6 +3,6 @@ package tdd.learn.card.user;
 import org.testcontainers.utility.DockerImageName;
 public class PostgreSQLTestImages {
 
-    static DockerImageName POSTGRES_TEST_IMAGE = DockerImageName.parse("postgres:9.6.12");
+    static DockerImageName POSTGRES_TEST_IMAGE = DockerImageName.parse("postgres:latest");
 
 }
--- tdd-card-user/src/test/java/tdd/learn/card/user/StartTest.java
+++ tdd-card-user/src/test/java/tdd/learn/card/user/StartTest.java
@@ -1,13 +1,17 @@
 package tdd.learn.card.user;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.function.Supplier;
 
 @Testcontainers
 @SpringBootTest
@@ -23,20 +27,32 @@ public class StartTest {
             .withUsername("postgres")
             .withPassword("password")
             .withDatabaseName("test");
+//            .withExposedPorts(5432);
 
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
-        container.start();
-        registry.add("spring.datasource.url",container::getJdbcUrl);
+        container.getMappedPort(5432);
+        final String logs = container.getLogs();
+        System.out.println("$$$$$$$$$$\n" + logs + "@@@@@@@@@@@\n");
+
+
+//        registry.add("spring.datasource.host", container::getHost);
+//        registry.add("spring.datasource.port", container::getFirstMappedPort);
+
+        final Supplier<Object> getJdbcUrl = container::getJdbcUrl;
+        registry.add("spring.datasource.url", getJdbcUrl);
         registry.add("spring.datasource.username",container::getUsername);
-        registry.add("spring.datasource.password",container::getUsername);
+        registry.add("spring.datasource.password",container::getPassword);
     }
 
 
 
     @Test
     void contextLoads() {
+        container.start();
+        final String logs2 = container.getLogs();
+        System.out.println("$$$$$$$$$$\n" + logs2 + "@@@@@@@@@\n");
         System.out.println("CONTENT LOADS");
     }
 
